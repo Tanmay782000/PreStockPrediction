3ff5a2510047dab3ce1e2ce2fd4f60016f06d44b
Tanmay782000 <mehtatanmay11@gmail.com>
2026-01-17 21:32:46 +0530

    created folders and authentication code for token generation

---
+++ Services/api-node/db/dynamo.client.js
@@ -0,0 +1,24 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { GetCommand, PutCommand } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+const TABLE = process.env.TokenHistory;
+
+exports.getToken = async (username) => {
+  const res = await client.send(
+    new GetCommand({
+      TableName: TABLE,
+      Key: { username }
+    })
+  );
+  return res.Item;
+};
+
+exports.saveToken = async (username, token, ttl) => {
+  return client.send(
+    new PutCommand({
+      TableName: TABLE,
+      Item: { username, token, ttl }
+    })
+  );
+};
